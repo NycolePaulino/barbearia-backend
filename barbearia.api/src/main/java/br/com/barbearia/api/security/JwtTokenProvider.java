6cb6e1427f8e6072b39e8f57c8ab9b483d8417e1
Nycole Paulino <nycole.paulino@polygonsolucoes.com.br>
2025-11-10 21:53:03 -0300

    feat: Adds endpoints for available time slots and appointment cancellations.

--- barbearia.api/src/main/java/br/com/barbearia/api/security/JwtTokenProvider.java
+++ barbearia.api/src/main/java/br/com/barbearia/api/security/JwtTokenProvider.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
+import java.util.Map;
 
 @Component
 public class JwtTokenProvider {
@@ -28,6 +29,8 @@ public class JwtTokenProvider {
         // pega os dados do usuário (com o google)
         OAuth2User oauth2User = (OAuth2User) authentication.getPrincipal();
         String email = oauth2User.getAttribute("email"); // pega o email
+        String name = oauth2User.getAttribute("name"); // pega o nome
+        String picture = oauth2User.getAttribute("picture"); // pega a foto (do google)
 
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
@@ -38,6 +41,8 @@ public class JwtTokenProvider {
         // constrói o Token JWT
         return Jwts.builder()
                 .setSubject(email)
+                .claim("name", name)
+                .claim("picture", picture)
                 .setIssuedAt(new Date())
                 .setExpiration(expiryDate)
                 .signWith(key, SignatureAlgorithm.HS512)
